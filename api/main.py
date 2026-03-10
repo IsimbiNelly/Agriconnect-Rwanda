f23f90b000b9ce399ab8354f71f9a43cf029f67a
IsimbiNelly <i.assoumpta@alustudent.com>
2026-03-10 15:19:58 +0200

    feature uvicorn server setup and running on port 8000 with dummy data

---
+++ api/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import uvicorn
+from fastapi import FastAPI
+from routers.buyers.buyers import router as buyers_router
+
+app = FastAPI(
+    title="AgriConnect API",
+    description="API for AgriConnect platform to connect farmers with buyers and provide agricultural services.",
+    )
+
+app.include_router(buyers_router)
+
+@app.get("/")
+async def home():
+    """welcome function"""
+    return {
+        "message": "Welcome to AgriConnect platform"
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        port=8000,
+        reload=True
+    )
